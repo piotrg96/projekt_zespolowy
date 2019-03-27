f0cfe145d29007a6e596619243de434520209141
Mateusz <mateusz.czubkowski@gmail.com>
2019-03-27 11:02:38 +0100

    changepassword model

---
+++ AppName/Models/ChangePasswordModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppName.Models
+{
+    public class ChangePasswordModel
+    {
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string Id { get; set; }
+
+    }
+}

dda4f86814822acd4d3523451d2b25d850ba1486
PatrykBurkiewicz <arkowiec96@interia.pl>
2019-04-01 20:42:48 +0200

    dodanie modelu ogloszen

---
+++ AppName/Models/AdvertisementModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppName.Models
+{
+    public class AdvertisementModel
+    {
+      
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string NameAdvert { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+
+
+        public AdvertisementModel(Guid id, string title, string nameAdvert, string description, string type)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            NameAdvert = nameAdvert;
+            Type = type;
+        }
+
+
+    }
+}

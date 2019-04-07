3fa7e1728f28b97084a082ed3e04731abc7aa0c0
Mateusz <mateusz.czubkowski@gmail.com>
2019-04-07 11:50:54 +0200

    Category model, preparation for adding tables to database

---
+++ AppName/Models/CategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppName.Models
+{
+    public class CategoryModel
+    {
+        [Column(TypeName = "nvarchar(50)")]
+        public string Name { get; set; }
+    }
+}

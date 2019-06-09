ace0c181a3f7a516aadb1b8eda6471e1dd295785
bartosz.bukowski21 <bartosz.bukowski21@gmail.com>
2019-06-09 19:34:19 +0200

    css next

--- AppName/frontend/src/_components/Advertisement/AdvertisementView.jsx
+++ AppName/frontend/src/_components/Advertisement/AdvertisementView.jsx
@@ -69,14 +69,20 @@ class AdvertisementView extends React.Component {
                 </div>
             </div>
 			<form onSubmit={this.handleSubmit}>
-            <div className="text-center mb-3"> 
-            <button type="submit"><FaStarO  size={32}/></button>
-            </div>
+                <div className="text-center mb-3"> 
+                    <button type="submit"><FaStarO  size={32}/></button>
+                </div>
             </form>
-            <Link to={{ pathname: '/sendMessage', state: { users: this.state.user, advUser,}}} className="btn btn-primary btn-block py-1 mb-3">
-                Wyslij wiadomość!
-            </Link> 
-            <Link to="/" className="btn btn-primary btn-block py-1 mb-3">Powrót</Link>
+            <div className="row">
+                <div className="col-md-6">
+                    <Link to={{ pathname: '/sendMessage', state: { users: this.state.user, advUser,}}} className="btn btn-primary btn-block mb-3">
+                        Wyslij wiadomość!
+                    </Link> 
+                </div>
+                <div className="col-md-6">
+                    <Link to="/" className="btn btn-primary btn-block mb-3">Powrót</Link>
+                </div>
+            </div>
         </div>
         );
     }
